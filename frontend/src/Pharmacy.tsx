2214431410dc5f3d5b506500d3669a3787b5d7e3
Morning Dove Nelson <morningdhansen@gmail.com>
2024-07-29 09:49:17 -0600

    add basic Pharmacy.tsx

---
+++ frontend/src/Pharmacy.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pharmacy:React.FC = () => {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default Pharmacy;
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+import Pharmacy from "./Pharmacy";
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <h1>Pharmacy</h1>
+    </header>
+    <main>
+      <Pharmacy />
+    </main>
+    <footer>
+      <p>Created By: Morning Dove Nelson 2024</p>
+    </footer>
+    </>
+  )
+}
+
+export default App
