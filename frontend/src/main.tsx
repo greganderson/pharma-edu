6acaa8e0e79893bd014141a55753af4c6f266207
Greg Anderson <greg.anderson.cs@gmail.com>
2024-07-29 20:05:37 -0600

    Switch formatters and add a couple components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Home from "./Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </React.StrictMode>
+);
