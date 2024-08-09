8c4253b2f504ef22a6f6beb571a9c22e56f9fb63
Chris Cooley <ccooley@bonadmin.com>
2024-08-09 10:16:26 -0600

    Add styling to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Nav from "./components/Nav";
 import routes from "./routes";
+import DixieTechLogo from "./assets/DixieTechLogo.png";
 
 const Header: React.FC = () => {
   const location = useLocation();
-  const currentRoute = routes.find(route => route.path === location.pathname);
-  const title = currentRoute ? currentRoute.name : "Default Title";
+  const currentRoute = routes.find((route) => route.path === location.pathname);
+  const title = currentRoute ? currentRoute.name : "Home";
+  const isHomePage = location.pathname === "/";
 
-  return <h1>{title}</h1>;
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+      {!isHomePage && (
+        <img src={DixieTechLogo} alt="Dixie Tech Logo" className="small-logo" />
+      )}
+    </div>
+  );
 };
 
 const App: React.FC = () => {
