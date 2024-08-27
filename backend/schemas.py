1ec3b6ce4b9ca2276b64c822c7ffd8017f9e4ca2
Greg Anderson <greg.anderson.cs@gmail.com>
2024-08-27 11:19:07 -0600

    Update create routes to have a CreateRequest schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -12,6 +12,22 @@ class PatientBasicInfo(BaseModel):
     date_of_birth: date
 
 
+class PatientCreateRequest(BaseModel):
+    first_name: str
+    last_name: str
+    date_of_birth: date
+    street: str
+    city: str
+    state: State
+    zipcode: str
+    allergies: str = ""
+    insurance_name: str | None = None
+    insurance_member_id: str | None = None
+    insurance_group_number: str | None = None
+    insurance_rx_bin: str | None = None
+    insurance_rx_pcn: str | None = None
+
+
 class PatientCreateResponse(BaseModel):
     patient_id: int
 
@@ -32,6 +48,19 @@ class PatientUpdateRequest(BaseModel):
     insurance_rx_pcn: str | None = None
 
 
+class PrescriberCreateRequest(BaseModel):
+    first_name: str
+    last_name: str
+    prescriber_type: PrescriberType = PrescriberType.MD
+    street: str
+    city: str
+    state: State
+    zipcode: str
+    contact_number: str | None = None
+    dea: str | None = None
+    npi: str | None = None
+
+
 class PrescriberCreateResponse(BaseModel):
     prescriber_id: int
 
@@ -40,7 +69,10 @@ class PrescriberUpdateRequest(BaseModel):
     first_name: str | None = None
     last_name: str | None = None
     prescriber_type: PrescriberType | None = None
-    office_address_id: int | None = None
+    street: str | None = None
+    city: str | None = None
+    state: State | None = None
+    zipcode: str | None = None
     contact_number: str | None = None
     dea: str | None = None
     npi: str | None = None
@@ -51,6 +83,18 @@ class PrescriptionBasicInfo(BaseModel):
     first_name: str
     last_name: str
     date_of_birth: date
+
+
+class PrescriptionCreateRequest(BaseModel):
+    patient_id: int
+    prescriber_id: int
+    prescribed_date: date
+    rx_item_id: int
+    directions: str
+    quantity: int
+    quantity_dispensed: int
+    refills: int
+    tech_initials: str
 
 
 class PrescriptionCreateResponse(BaseModel):
@@ -68,6 +112,16 @@ class PrescriptionUpdateRequest(BaseModel):
     refills: int | None = None
     status: PrescriptionStatus | None = None
     tech_initials: str | None = None
+
+
+class RxItemCreateRequest(BaseModel):
+    name: str
+    strength: str
+    ndc: str
+    expiration: date | None = None
+    lot_number: str
+    dea_schedule: str | None = None
+    drug_class: str | None = None
 
 
 class RxItemCreateResponse(BaseModel):
