6c9866bce8451f214d7e7e389144dac52ebb2a83
Morning Dove Nelson <morningdhansen@gmail.com>
2024-08-28 09:10:44 -0600

    correct typo

--- frontend/src/pages/Prescriber/UpdatePrescriber.tsx
+++ frontend/src/pages/Prescriber/UpdatePrescriber.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import { Prescriber, states } from './PrescriberModels';
 import styles from "./AddPrescriber.module.css";
