adf9cda2812227f4a4686b65eafc0440bb23c4ba
Morning Dove Nelson <morningdhansen@gmail.com>
2024-08-29 10:38:59 -0600

    sync backend

--- backend/models.py
+++ backend/models.py
@@ -113,18 +113,19 @@ class Patient(SQLModel, table=True):
     first_name: str
     last_name: str
     date_of_birth: date
+    phone_number: str
     street: str
     city: str
     state: State
     zipcode: str
     allergies: str = ""
-    # phone_number: str
     prescriptions: list["Prescription"] = Relationship(back_populates="patient")
     insurance_name: str | None = None
     insurance_member_id: str | None = None
     insurance_group_number: str | None = None
     insurance_rx_bin: str | None = None
     insurance_rx_pcn: str | None = None
+    insurance_person_code: str
 
 
 class Prescriber(SQLModel, table=True):
@@ -132,7 +133,6 @@ class Prescriber(SQLModel, table=True):
     first_name: str
     last_name: str
     prescriber_type: PrescriberType = PrescriberType.MD
-    # facility: str
     street: str
     city: str
     state: State
@@ -164,7 +164,7 @@ class RxItem(SQLModel, table=True):
     name: str
     strength: str
     ndc: str
-    expiration: date | None = None
+    expiration: date
     lot_number: str
+    dosage_form: str
     dea_schedule: str | None = None
-    drug_class: str | None = None
